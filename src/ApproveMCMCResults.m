8a7fec251d0d66e94cbc2f7447772cb7f89cee8c
Maximilian Gehri <maximilian.gehri@tu-darmstadt.de>
2023-07-28 20:04:08 +0200

    Adapt ApproveMCMCResults.m to modified export structure with possibly multiple elongation rates. All functions that were functional in the old version are still functional.

--- src/ApproveMCMCResults.m
+++ src/ApproveMCMCResults.m
@@ -1,8 +1,9 @@
 function ApproveMCMCResults(varargin)
 
-%Analyzes saved MCMC results of 2 color elongation data. The user has the option of
-%approving or rejecting the results of each single nucleus fit. The
-%approved/rejected results are saved in the same .mat file as the MCMC results.
+%Analyzes saved MCMC results of 2 color elongation data of a particular
+%construct (specified in the library).The user has the option of approving
+%or rejecting the results of each single nucleus fit. The approved/rejected
+%results are saved in the same .mat file as the MCMC results.
 
 %The MCMC results should be saved as a .mat file with 2 stuctures, MCMCplot
 %and MCMCresults. Each should be of size 1xN, where N is the number of
@@ -19,11 +20,15 @@ function ApproveMCMCResults(varargin)
 %   'TerminationModel': analyze termination model results
 %   'InitialRise': analyze initial rise analysis results
 %   'WholeCycle': analyze whole cycle analysis results
-%   'LoadPrevious': load ApprovedFits from previous analysis to save into
-%   this one
+%   'LoadPrevious': load ApprovedFits from previous analysis of the same
+%    dataset to save into this one
 %   'nc13': analyze nc13
 %   'nc14': analyze nc14
 %   'RawChains': view raw MCMC chains
+%   'SmoothRate': percentage of loaded datapoints to smooth the loading
+%   rate
+%   'fileDir': specify file directory. Otherwise a dialog box will open to
+%   let you choose the directory.
 
 %% Input arguments
 %By default, user select which datasets to load
@@ -66,8 +71,16 @@ for i=1:length(varargin)
         load_previous = true;
     end
 end
+
 %% Load results
-files = dir('S:\Jonathan\Dropbox\2 Color Elongation\MCMC Results\DwellModel\*.mat');
+if strcmpi(varargin{i},'fileDir')
+    fileDir = varargin{i+1};
+else
+    msg_box = msgbox('Choose the directory, that contains the MCMC results.', 'Directory Selection'); % Display a message box with instructions
+    uiwait(msg_box); % Halt execution (make the figure modal) until the OK button is clicked on the message box
+    fileDir = uigetdir(pwd); % Open dialog box to choose the directory
+end
+files = dir(fileDir);
 names = {files.name};
 
 %Check for analysis type and nuclear cycle.
@@ -108,37 +121,81 @@ if isempty(s_all)
 end
 
 %Choose which results to load
-[s,v] = listdlg('PromptString','Select a dataset:','SelectionMode',...
-'single','ListString',names(s_all),'ListSize',[360 300]); %s is the index of the chosen results
+[s,~] = listdlg('PromptString','Select a dataset:','SelectionMode',...
+    'single','ListString',names(s_all),'ListSize',[360 300]); %s is the index of the chosen results
 
-%Load data
-m = matfile([files(s_all(s)).folder,'\',char(names(s_all(s)))],...
-    'Writable',true); %MCMC results .mat file
-vars = whos(m); %Unpack the variables
+%Load data: Create a matfile object of the chosen dataset (MCMC results). This object
+%allows for accessing and changing parts of a .mat file stored on disk
+%without loading the whole file into memory.
+m = matfile(fullfile(files(s_all(s)).folder,char(names(s_all(s))),filesep),...
+    'Writable',true);
+vars = whos(m); %Unpack the variables of m
 varnames = {vars.name};
 N = length(m.MCMCplot); %Number of nuclei fits in this dataset
 
 %Extract the data into temporary variables.
 MCMCplot = m.MCMCplot;
 MCMCresults = m.MCMCresults;
+
+% In case of RawChain option, look for *_RawChain.mat file in fileDir and
+% fileDir/Raw Chains/. Otherwise open dialog box to choose the file. 
 if RawChains
-    chain_name = char(names(s_all(s)));
-    chains = load([files(s_all(s)).folder,'\Raw Chains\',chain_name(1:end-4),'_RawChain.mat']);
+    chain_file = fullfile(files(s_all(s)).folder,'Raw Chains',[names{s_all(s)}(1:end-4),'_RawChain.mat']);
+    if exist(chain_file, 'file') == 2
+        %disp('File exists')
+        chains = load(chain_file);
+    else
+        chain_file = fullfile(files(s_all(s)).folder,[names{s_all(s)}(1:end-4),'_RawChain.mat']);
+        if exist(chain_file, 'file') == 2
+            %disp('File exists')
+            chains = load(chain_file);
+        else
+            %disp('File does not exist')
+            msg_box = msgbox(['Choose the mat-file containing the raw chains assciated with ',names{s_all(s)}(1:end-4),'.'], 'File Selection'); % Display a message box with instructions
+            uiwait(msg_box); % Halt execution (make the figure modal) until the OK button is clicked on the message box
+            [chain_file,chain_path] = uigetfile(fullfile(files(s_all(s)).folder, '*_RawChain.mat'));
+            if contains(chain_file,names{s_all(s)}(1:end-4))
+                chains = load([chain_path,chain_file]);
+            else
+                error('You picked the wrong file for the RawChains option.');
+            end
+        end
+    end
+    % Extract the chains into temporary variable.
     MCMCchain = chains.MCMCchain;
 end
 
 %If loading previous ApprovedFits, choose file to load
 if load_previous
-    [file,path] = uigetfile('S:\Jonathan\Dropbox\2 Color Elongation\MCMC Results\DwellModel\*.mat');
+    [file,path] = uigetfile(pwd,'Choose the dataset containing information on previous approval.');
     prev = load([path,file]);
     MCMCresults_previous = prev.MCMCresults;
     if length(MCMCresults) ~= length(MCMCresults_previous)
-        error('Please select previous analysis of the same dataset');
+        error('Please select previous analysis of the same dataset.');
     end
     for i = 1:length(MCMCresults_previous)
         MCMCresults(i).ApprovedFits = MCMCresults_previous(i).ApprovedFits;
     end
 end
+
+%% Load construct details
+
+% Extract construct name from dataset (assuming that all cells in the set
+% are of the same kind)
+construct = MCMCresults(1).FittedConstruct;
+
+%Query to the construct library regarding the construct details:
+%'segments' and 'velocities' contain details about the elongation rates on
+%different segments of the construct and the length of the construct
+[ElongationSegments,~,x_drop] = library(construct);
+
+%Extract distinguishable names of velocity parameters
+velocity_names = unique(ElongationSegments.velocities);
+
+% Generate field names of velocity parameters
+velocity_fields_MCMCresults = [cellfun(@(x) strcat('mean_',x), velocity_names, 'UniformOutput', false),...
+    cellfun(@(x) strcat('CI_',x), velocity_names, 'UniformOutput', false)];
+%[fields_params,fields_MCMCchain,fields_MCMCresults] = generateExportStructures(1,velocity_names,x_drop);
 
 %% Approve/reject fits
 
@@ -152,7 +209,7 @@ t = table(InputKey,Function);
 colormap = {'red',[0.94 0.94 0.94],'green'}; %Approved/Uncurated/Rejected colors
 
 running = true; %Keep running program until stopped
-i = 1; %Start with first indexed nucleus for now
+cellNum = 1; %Start with first indexed nucleus for now
 
 close all
 
@@ -161,173 +218,185 @@ if RawChains
     c = figure('Name','Parameter distributions','Position',[600 100 600 600]);
 else
     f = figure('Name','Inference Results','Position',[200 100 800 600]);
-    
+
 end
 while running
-    
-clf(f); %Clear figure
-if RawChains
-    clf(c);
-end
 
-%Extract plotting variables from results
-t_plot = MCMCplot(i).t_plot;
-MS2_plot = MCMCplot(i).MS2_plot;
-PP7_plot = MCMCplot(i).PP7_plot;
-%t_interp = MCMCplot(i).t_interp;
-%MS2_interp = MCMCplot(i).MS2_interp;
-%PP7_interp = MCMCplot(i).PP7_interp;
-simMS2 = MCMCplot(i).simMS2;
-simPP7 = MCMCplot(i).simPP7;
-%MeanAP = MCMCplot(i).MeanAP;
-
-%Get fit results
-mean_v = MCMCresults(i).mean_v;
-sigma_v = MCMCresults(i).sigma_v;
-mean_R0 = MCMCresults(i).mean_R(1);
-sigma_R0 = MCMCresults(i).sigma_R(1);
-mean_dR = MCMCresults(i).mean_R(2:end);
-sigma_dR = MCMCresults(i).sigma_R(2:end);
-mean_ton = MCMCresults(i).mean_ton;
-sigma_ton = MCMCresults(i).sigma_ton;
-mean_dwelltime = MCMCresults(i).mean_dwell;
-sigma_dwelltime = MCMCresults(i).sigma_dwell;
-
-%Smoothed loading rate
-rate_plot = mean_R0 + mean_dR;
-rateerror_plot = sigma_R0 + sigma_dR;
-ratesmooth_plot = smooth(rate_plot,span);
-
-%Remove loading rates before inferred initiation time
-remove_times = find(t_plot(1:end-1) < mean_ton);
-rate_plot(remove_times) = nan;
-rateerror_plot(remove_times) = nan;
-ratesmooth_plot(remove_times) = nan;
-
-%Raw chain results
-if RawChains
-    dwelltime_chain = MCMCchain(i).dwell_chain; %termination dwell time
-    R0_chain = MCMCchain(i).R_chain(:,1); %mean loading rate
-    dR_chain = MCMCchain(i).R_chain(:,2:end); %last loading rate fluctuation 
-end
-
-%% Plot fit results
-
-%subplot(2,1,1)
-
-hold on
-plot(t_plot,MS2_plot,'ro','DisplayName','MS2 data');
-plot(t_plot,PP7_plot,'go','DisplayName','PP7 data');
-%plot(t_interp,MS2_interp,'r-','DisplayName','MS2 interpolation');
-%plot(t_interp,PP7_interp,'g-','DisplayName','PP7 interpolation');
-plot(t_plot,simMS2,'k--','DisplayName','MS2 fit');
-plot(t_plot,simPP7,'k--','DisplayName','PP7 fit');
-
-
-xlim([t_plot(1), t_plot(end)*1.3]);
-ylim([0,  max(PP7_plot) * 1.2]);
-xlabel('Time since nuclear cycle start (min)');
-ylabel('Fluorescence (AU)');
-legend('Location','Northwest')
-title({['Single nucleus fit ',num2str(i),' of ',num2str(N)]...
-    ['Elongation rate = ',num2str(mean_v), ' +/- ',num2str(sigma_v), 'kb/min']});
-f.Color = colormap{MCMCresults(i).ApprovedFits+2}; %Set color depending on approval status
-
-%Plot inferred rate
-%subplot(2,1,2);
-%hold on
-%errorbar(t_plot(1:end-1),rate_plot,rateerror_plot,'r.-','CapSize',0,...
-%    'DisplayName','Inferred loading rate');
-%plot(t_plot(1:end-1),ratesmooth_plot,'k--','DisplayName','Smoothed loading rate');
-%line(xlim,mean_R0(1)*[1,1],'Color','black','LineStyle','-','DisplayName',...
-%    'Inferred mean loading rate');
-%hold off
-
-%xlim([t_plot(1), t_plot(end)*1.3]);
-%ylim([0,  max(MS2_plot) * 1.2]);
-%line(mean_ton*[1,1],ylim,'Color','blue','LineStyle','--',...
-%    'DisplayName','Inferred time on');
-%xlabel('Time since nuclear cycle start (min)');
-%ylabel('Loading rate (AU/min)');
-%legend('Location','southeast');
-
-%Plot raw chains if desired
-if RawChains
-    figure(c);
-    hold on;
-
-    subplot(3,2,1)
-    histogram(dwelltime_chain);
-    xlabel('Dwell time (min)');
-
-    subplot(3,2,2)
-    plot(dwelltime_chain,'b.');
-    ylabel('Dwell time(min)');
-
-    subplot(3,2,3)
-    histogram(R0_chain);
-    xlabel('Mean loading rate (AU/min)');
-
-    subplot(3,2,4)
-    plot(R0_chain,'b.');
-    ylabel('Mean loading rate (AU/min)');
-
-    subplot(3,2,5)
-    histogram(dR_chain(:,end));
-    xlabel('Last loading rate fluctuation (AU/min)');
-
-    subplot(3,2,6)
-    plot(dR_chain(:,end),'b.');
-    ylabel('Last loading rate fluctuation (AU/min)');
-end
-
-% User options (approve/reject, change nucleus)
-disp(t); %Display options
-exitflag = false; %Loop keypress query until valid exit keypress
-
-while ~exitflag
-figure(f);
-waitforbuttonpress; %User input to press a key
-key = f.CurrentCharacter; %Last pressed key
-
-if strcmp(key,'a')
-    MCMCresults(i).ApprovedFits = 1;
-    disp('Approved');
-elseif strcmp(key,'r')
-    MCMCresults(i).ApprovedFits = -1;
-    disp('Rejected');
-elseif strcmp(key,',')
-    if i > 1
-        i = i - 1;
-        disp('Switching to previous nucleus');
-        exitflag = true;
-    elseif i == 1
-        disp('Already at first nucleus!');
+    clf(f); %Clear figure
+    if RawChains
+        clf(c);
     end
-elseif strcmp(key,'.')
-    if i < N
-        i = i + 1;
-        disp('Switching to next nucleus');
-        exitflag = true;
-    elseif i == N
-        disp('Already at last nucleus!');
+
+    %Extract plotting variables from results
+    t_plot = MCMCplot(cellNum).t_plot;
+    MS2_plot = MCMCplot(cellNum).MS2_plot;
+    PP7_plot = MCMCplot(cellNum).PP7_plot;
+    %t_interp = MCMCplot(cellNum).t_interp;
+    %MS2_interp = MCMCplot(cellNum).MS2_interp;
+    %PP7_interp = MCMCplot(cellNum).PP7_interp;
+    simMS2 = MCMCplot(cellNum).simMS2;
+    simPP7 = MCMCplot(cellNum).simPP7;
+    %MeanAP = MCMCplot(cellNum).MeanAP;
+
+    %Get fit results of elongation rates, R, dR, ton and tau
+
+    N_idx=length(velocity_fields_MCMCresults)/2;
+    mean_velocity = zeros(1,N_idx); %Generate vector of mean parameters
+    for idx=1:N_idx
+        mean_velocity(idx) = MCMCresults(cellNum).(velocity_fields_MCMCresults{idx});   %Extract and save means of the velocities
+        CI_velocities(:,idx) = MCMCresults(cellNum).(velocity_fields_MCMCresults{N_idx+idx}); %Extract and save credible interval of the velocities
     end
-elseif strcmp(key,'j')
-    j = input('Enter in nucleus number to jump to:');
-    if j >= 1 && j <= N
-        i = j;
-        disp(['Switching to nucleus ',num2str(i)]);
-        exitflag = true;
-    else
-        disp(['Error: please enter an integer between 1 and ',num2str(N)]);
+
+    mean_R = MCMCresults(cellNum).mean_R;
+    sigma_R = MCMCresults(cellNum).CI_R; %TODO
+    mean_dR = MCMCresults(cellNum).mean_R;
+    sigma_dR = MCMCresults(cellNum).CI_dR; %TODO
+    mean_ton = MCMCresults(i).mean_ton;
+    sigma_ton = MCMCresults(i).CI_ton; %TODO
+    mean_dwelltime = MCMCresults(i).mean_tau;
+    sigma_dwelltime = MCMCresults(i).CI_tau; %TODO
+
+    %Smoothed loading rate
+    rate_plot = mean_R + mean_dR;
+    rateerror_plot = sigma_R + sigma_dR;
+    ratesmooth_plot = smooth(rate_plot,span);
+
+    %Remove loading rates before inferred initiation time
+    remove_times = find(t_plot(1:end-1) < mean_ton);
+    rate_plot(remove_times) = NaN;
+    rateerror_plot(remove_times) = NaN;
+    ratesmooth_plot(remove_times) = NaN;
+
+    %Raw chain results
+    if RawChains
+        dwelltime_chain = MCMCchain(cellNum).tau_chain; %termination dwell time
+        R_chain = MCMCchain(cellNum).R_chain; %mean loading rate
+        dR_chain = MCMCchain(cellNum).R_chain; %last loading rate fluctuation
     end
-elseif strcmp(key,'x')
-    disp('Exiting and saving results')
-    exitflag = true;
-    running = 0;
-end
-f.Color = colormap{MCMCresults(i).ApprovedFits+2}; %Set color depending on approval status
-end
+
+    %% Plot fit results
+
+    %subplot(2,1,1)
+    figure(f);
+    hold on
+    plot(t_plot,MS2_plot,'ro','DisplayName','MS2 data');
+    plot(t_plot,PP7_plot,'go','DisplayName','PP7 data');
+    %plot(t_interp,MS2_interp,'r-','DisplayName','MS2 interpolation');
+    %plot(t_interp,PP7_interp,'g-','DisplayName','PP7 interpolation');
+    plot(t_plot,simMS2,'k--','DisplayName','MS2 fit');
+    plot(t_plot,simPP7,'k--','DisplayName','PP7 fit');
+
+
+    xlim([t_plot(1), t_plot(end)*1.3]);
+    ylim([0,  max(PP7_plot) * 1.2]);
+    xlabel('Time since nuclear cycle start (min)');
+    ylabel('Fluorescence (AU)');
+    legend('Location','Northwest');
+    % Generate title with multiple lines (displaying a list of all elongation rates fit falues)
+    title_array = cell(1,N_idx+2);
+    title_array{1} = ['Single nucleus fit ',num2str(cellNum),' of ',num2str(N)];
+    title_array{2} = '-------------------------------------------------------------------';
+    for idx=1:N_idx
+        title_array{idx+2} = ['Elongation rate ',velocity_names{idx},' = ',num2str(mean_velocity(idx)),' kb/min', ' (95% CI: [',num2str(CI_velocities(1,idx)),',',num2str(CI_velocities(2,idx)),'])'];
+    end
+    title(title_array);
+    f.Color = colormap{MCMCresults(cellNum).ApprovedFits+2}; %Set color depending on approval status
+
+    %Plot inferred rate
+    %subplot(2,1,2);
+    %hold on
+    %errorbar(t_plot(1:end-1),rate_plot,rateerror_plot,'r.-','CapSize',0,...
+    %    'DisplayName','Inferred loading rate');
+    %plot(t_plot(1:end-1),ratesmooth_plot,'k--','DisplayName','Smoothed loading rate');
+    %line(xlim,mean_R*[1,1],'Color','black','LineStyle','-','DisplayName',...
+    %    'Inferred mean loading rate');
+    %hold off
+
+    %xlim([t_plot(1), t_plot(end)*1.3]);
+    %ylim([0,  max(MS2_plot) * 1.2]);
+    %line(mean_ton*[1,1],ylim,'Color','blue','LineStyle','--',...
+    %    'DisplayName','Inferred time on');
+    %xlabel('Time since nuclear cycle start (min)');
+    %ylabel('Loading rate (AU/min)');
+    %legend('Location','southeast');
+
+    %Plot raw chains if desired
+    if RawChains
+        figure(c);
+        hold on;
+
+        subplot(3,2,1)
+        histogram(dwelltime_chain);
+        xlabel('Dwell time (min)');
+
+        subplot(3,2,2)
+        plot(dwelltime_chain,'b.');
+        ylabel('Dwell time(min)');
+
+        subplot(3,2,3)
+        histogram(R_chain);
+        xlabel('Mean loading rate (AU/min)');
+
+        subplot(3,2,4)
+        plot(R_chain,'b.');
+        ylabel('Mean loading rate (AU/min)');
+
+        subplot(3,2,5)
+        histogram(dR_chain(:,end));
+        xlabel('Last loading rate fluctuation (AU/min)');
+
+        subplot(3,2,6)
+        plot(dR_chain(:,end),'b.');
+        ylabel('Last loading rate fluctuation (AU/min)');
+    end
+
+    % User options (approve/reject, change nucleus)
+    disp(t); %Display options
+    exitflag = false; %Loop keypress query until valid exit keypress
+
+    while ~exitflag
+        figure(f);
+        waitforbuttonpress; %User input to press a key
+        key = f.CurrentCharacter; %Last pressed key
+
+        if strcmp(key,'a')
+            MCMCresults(cellNum).ApprovedFits = 1;
+            disp('Approved');
+        elseif strcmp(key,'r')
+            MCMCresults(cellNum).ApprovedFits = -1;
+            disp('Rejected');
+        elseif strcmp(key,',')
+            if cellNum > 1
+                cellNum = cellNum - 1;
+                disp('Switching to previous nucleus');
+                exitflag = true;
+            elseif cellNum == 1
+                disp('Already at first nucleus!');
+            end
+        elseif strcmp(key,'.')
+            if cellNum < N
+                cellNum = cellNum + 1;
+                disp('Switching to next nucleus');
+                exitflag = true;
+            elseif cellNum == N
+                disp('Already at last nucleus!');
+            end
+        elseif strcmp(key,'j')
+            j = input('Enter in nucleus number to jump to:');
+            if j >= 1 && j <= N
+                cellNum = j;
+                disp(['Switching to nucleus ',num2str(cellNum)]);
+                exitflag = true;
+            else
+                disp(['Error: please enter an integer between 1 and ',num2str(N)]);
+            end
+        elseif strcmp(key,'x')
+            disp('Exiting and saving results')
+            exitflag = true;
+            running = 0;
+        end
+        f.Color = colormap{MCMCresults(cellNum).ApprovedFits+2}; %Set color depending on approval status
+    end
 
 end
 
